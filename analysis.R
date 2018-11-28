d2af4b8b97e369fc22220081dfc3670d2569deda
arcticdreamz <tomcrasset@gmail.com>
2018-11-28 17:31:39 +0100

    Compute robust distances and create dd-plot and robust cor matrix

--- analysis.R
+++ analysis.R
@@ -1,5 +1,5 @@
-# setwd("~/Documents/Master1_DataScience/1er QUADRI/High_Dimensional_Data_Analysis/Breast-cancer-data-analysis")
-setwd("~/Documents/INGE/MASTER/1ère\ MASTER/1er\ QUADRI/HDDA/Projects/Breast-cancer-data-analysis/")
+setwd("~/Documents/Master1_DataScience/1er QUADRI/High_Dimensional_Data_Analysis/Breast-cancer-data-analysis")
+#setwd("~/Documents/INGE/MASTER/1ère\ MASTER/1er\ QUADRI/HDDA/Projects/Breast-cancer-data-analysis/")
 
 # Data loading
 data <- read.table("data.csv", header=TRUE, sep=',')
@@ -26,6 +26,7 @@ boxplot(data[,7] ~ Classification, main='Adiponectin levels')
 boxplot(data[,8] ~ Classification, main='Resistin levels')
 boxplot(data[,9] ~ Classification, main='MCP.1 levels')
 
+
 # Matrix of scatterplots of the quantitative features
 pairs(data[,1:9])
 
@@ -33,5 +34,31 @@ pairs(data[,1:9])
 c <- cor(data)
 library(corrplot)
 corrplot(c)
+
+
+# Mahalanobis distances and multivariate 
+m <- apply(data[,1:9], 2, mean)
+S <- cov(data[,1:9])
+maha <- mahalanobis(data[,1:9], m, S)
+
+#Plotting standard distances (not robust)
+plot(maha, type='h')
+abline(h= qchisq(0.95, 9), col='red')  # p = 9 = number of quantitative variables
+#Robust estimator
+library(MASS)
+robS = cov.rob(data[,1:9], quantile.used = floor((116 + 9 + 1)/2), method='mcd',cor = TRUE)
+robust_dist<- mahalanobis(data[,1:9], robS$center, robS$cov)
+
+#Plotting robust distances
+plot(robust_dist, type = "h")
+abline(h=qchisq(0.975,9), col="red")
+
+#DDplot
+plot(maha,robust_dist)
+
+#Robust correlation matrix
+corrplot(robust_est$cor)
+
+
 
 detach(data)
